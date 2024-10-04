c96d01c8686592c4553c4a1cdaa5e1fe0777e6fb
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-04 13:17:28 -0300

    Console project setup and starter code demo
    Added reference to DataAccess and Models projects to console
    project. Added code to check if database and/or tables exists and
    it initializes any part of it if needed. Check for pending
    migrations, apply missing migrations if pending.

--- CodingWiki.Console/Program.cs
+++ CodingWiki.Console/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CodingWiki.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, Coding Wiki JS!");
 Console.WriteLine("JS does not stand for JavaScript ;)");
+
+using ApplicationDbContext context = new();
+context.Database.EnsureCreated();
+
+if (context.Database.GetPendingMigrations().Any())
+{
+    context.Database.Migrate();
+}
