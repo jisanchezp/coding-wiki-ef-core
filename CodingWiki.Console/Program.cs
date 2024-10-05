86edc25808765cf84ba06a66e899fa09108ae511
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-04 22:12:10 -0300

    Implemented GetAllBooks

--- CodingWiki.Console/Program.cs
+++ CodingWiki.Console/Program.cs
@@ -5,10 +5,23 @@ using Microsoft.EntityFrameworkCore;
 Console.WriteLine("Hello, Coding Wiki JS!");
 Console.WriteLine("JS does not stand for JavaScript ;)");
 
-using ApplicationDbContext context = new();
-context.Database.EnsureCreated();
+//using ApplicationDbContext context = new();
+//context.Database.EnsureCreated();
 
-if (context.Database.GetPendingMigrations().Any())
+//if (context.Database.GetPendingMigrations().Any())
+//{
+//    context.Database.Migrate();
+//}
+
+GetAllBooks();
+
+void GetAllBooks()
 {
-    context.Database.Migrate();
+    using ApplicationDbContext context = new();
+    var books = context.Books.ToList();
+
+    foreach (var book in books)
+    {
+        Console.WriteLine($"{book.Title} - {book.ISBN}");
+    }
 }
