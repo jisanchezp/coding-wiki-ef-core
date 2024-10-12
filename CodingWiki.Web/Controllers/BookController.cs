efedea568f2c756450fbadb684d0a0db454b8d8f
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-12 17:36:08 -0300

    Add BookController w/Index

---
+++ CodingWiki.Web/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using CodingWiki.DataAccess.Data;
+using CodingWiki.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> books = _db.Books.ToList();
+
+            return View(books);
+        }
+    }
+}
