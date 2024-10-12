6cffc1383ddd157d2956b667808b3262bff4b283
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-12 14:25:27 -0300

    Added Publisher controller and Index action/view

---
+++ CodingWiki.Web/Controllers/PublisherController.cs
@@ -0,0 +1,23 @@
+﻿using CodingWiki.DataAccess.Data;
+using CodingWiki.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki.Web.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PublisherController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Publisher> publishers = _db.Publishers.ToList();
+
+            return View(publishers);
+        }
+    }
+}
