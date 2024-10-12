6f950c0b3791b2a6727590a53c2a268f970bdac9
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-12 14:16:42 -0300

    Added author controller and index view

---
+++ CodingWiki.Web/Controllers/AuthorController.cs
@@ -0,0 +1,22 @@
+﻿using CodingWiki.DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki.Web.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AuthorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var authors = _db.Authors.ToList();
+
+            return View(authors);
+        }
+    }
+}
