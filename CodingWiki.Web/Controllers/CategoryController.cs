813613c86e3c6e24b9b0089545253f8edfc16957
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-12 12:44:09 -0300

    Add category controller and index view

---
+++ CodingWiki.Web/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using CodingWiki.DataAccess.Data;
+using CodingWiki.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodingWiki.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
