44ca3dd9ba00179ccf2449f437d92b801229f65b
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-12 17:50:40 -0300

    Added Book ViewModel

---
+++ CodingWiki.Model/ViewModels/BookVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki.Model.ViewModels
+{
+    public class BookVM
+    {
+        public BookVM Book { get; set; }
+        public IEnumerable<SelectListItem> PublisherList { get; set; }
+    }
+}
