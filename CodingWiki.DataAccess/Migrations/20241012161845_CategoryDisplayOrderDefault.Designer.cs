97f8e947c819434c10bf31a2bb011ae8428c78d5
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-12 13:26:30 -0300

    Modified DisplayOrder default value
    Added migration to set Category DisplayOrder
    default value to 0.

--- CodingWiki.DataAccess/Migrations/20241012161845_CategoryDisplayOrderDefault.Designer.cs
+++ CodingWiki.DataAccess/Migrations/20241012161845_CategoryDisplayOrderDefault.Designer.cs
@@ -4,6 +4,7 @@ using CodingWiki.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CodingWiki.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241012161845_CategoryDisplayOrderDefault")]
+    partial class CategoryDisplayOrderDefault
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
